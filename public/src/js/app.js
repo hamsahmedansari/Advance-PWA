f27ad7e45a8d913578610b15e2b731f0a7928760
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-16 18:10:01 +0500

    UPDATE: :countruction: Add To Home Banner is firing

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,15 @@
+var deferredPrompt;
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/sw.js")
     .then(() => console.log("[Service Worker] Registerr"))
     .catch(err => console.error("[Service Worker] has Error", err));
 }
+
+// Before Installing/Adding to home this event will fire
+window.addEventListener("beforeinstallprompt", e => {
+  console.log("[AddToHome] before installing prompt is fired ...", e);
+  e.preventDefault();
+  deferredPrompt = e;
+  return false; //false for not installing on chrome smart time
+});
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,26 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.the(result => {
+      console.log("[OpenCreatePostButton] is fired ...", result);
+      if (result.outcome === "dismissed")
+        console.log("[OpenCreatePostButton] User Canalled prompt");
+      else console.log("[OpenCreatePostButton] User Added to Home");
+    });
+  }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
