e9fd0d30b84ffe840fd12d56940f9154e9e161c1
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-16 19:05:42 +0500

    UPDATE: :countruction: add allFiles to caches

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,24 @@
 // install
 self.addEventListener("install", e => {
   console.log("[Service Worker] is installing ...", e);
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      console.log("[Service Worker] PreCaching is Start");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      ]);
+    })
+  );
 });
 // Activating
 self.addEventListener("activate", e => {
@@ -8,5 +26,10 @@ self.addEventListener("activate", e => {
 });
 // Fetch - its fetch all request i.e HTML,CSS,IMAGE,fetch from server
 self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      if (res) return res;
+      else return fetch(e.request);
+    })
+  );
 });
