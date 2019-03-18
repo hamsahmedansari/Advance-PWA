1717382a2362ef566ad2592dcb7b09c83001c7e4
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-18 16:45:38 +0500

    UPDATE: :construction: First Cache Then Network Overwite Cache  if Network success in selected request from the server then it overwite the view and cache of that request only and serving all static file from cache

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,26 @@
 var STATIC_CACHE = "static-v6";
 var DYNAMIC_CACHE = "dynamic-v2";
+var STATIC_FILES = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/src/js/app.js",
+  "/src/js/feed.js",
+  "/src/js/material.min.js",
+  "/src/css/app.css",
+  "/src/css/feed.css",
+  "/src/images/main-image.jpg",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+];
 // install
 self.addEventListener("install", e => {
   console.log("[Service Worker] is installing ...", e);
   e.waitUntil(
     caches.open(STATIC_CACHE).then(cache => {
       console.log("[Service Worker] PreCaching is Start");
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/offline.html",
-        "/src/js/app.js",
-        "/src/js/feed.js",
-        "/src/js/material.min.js",
-        "/src/css/app.css",
-        "/src/css/feed.css",
-        "/src/images/main-image.jpg",
-        "https://fonts.googleapis.com/css?family=Roboto:400,700",
-        "https://fonts.googleapis.com/icon?family=Material+Icons",
-        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
-      ]);
+      cache.addAll(STATIC_FILES);
     })
   );
 });
@@ -54,6 +55,10 @@ self.addEventListener("fetch", e => {
         });
       })
     );
+  } else if (
+    new RegExp("\\b" + STATIC_FILES.join("\\b|\\b") + "\\b").test(e.request.url)
+  ) {
+    e.respondWith(caches.match(e.request));
   } else {
     e.respondWith(
       caches.match(e.request).then(res => {
