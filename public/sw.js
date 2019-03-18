125f955e0a0ebf0b53617c2a9764c0ff98683ff9
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-18 14:50:24 +0500

    UPDATE: :tada: ServiceWoker Basic Caches Setup build Static and Dynamic Caches with version's

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
+var STATIC_CACHE = "static-v1";
+var DYNAMIC_CACHE = "dynamic-v1";
 // install
 self.addEventListener("install", e => {
   console.log("[Service Worker] is installing ...", e);
   e.waitUntil(
-    caches.open("static").then(cache => {
+    caches.open(STATIC_CACHE).then(cache => {
       console.log("[Service Worker] PreCaching is Start");
       cache.addAll([
         "/",
@@ -23,13 +25,31 @@ self.addEventListener("install", e => {
 // Activating
 self.addEventListener("activate", e => {
   console.log("[Service Worker] is activating ...", e);
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== STATIC_CACHE && key !== DYNAMIC_CACHE) {
+            console.log("[SERVICE WORKER] Deleting Old Cache ...", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 // Fetch - its fetch all request i.e HTML,CSS,IMAGE,fetch from server
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(res => {
       if (res) return res;
-      else return fetch(e.request);
+      else
+        return fetch(e.request).then(response => {
+          return caches.open(DYNAMIC_CACHE).then(cache => {
+            cache.put(e.request.url, response.clone());
+            return response;
+          });
+        });
     })
   );
 });
