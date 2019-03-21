ada4a383cbbd7e372efbeb51ca58ade9cb94c7e8
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-21 17:45:23 +0500

    serve install

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.use("/", express.static("public"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App is running on Port ${port}`));
