d2ebb04df823b7e19ce52419e242b12796036a4f
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-21 16:20:05 +0500

    UPDATE: :countruction: Notification Created From Service Worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -11,6 +11,26 @@ if ("serviceWorker" in navigator) {
 }
 
 function createNotification(title, body = null) {
+  // if (Notification.permission == 'granted') {
+  //   navigator.serviceWorker.getRegistration().then(function(reg) {
+  //     var options = {
+  //       body: 'Here is a notification body!',
+  //       icon: 'images/example.png',
+  //       vibrate: [100, 50, 100],
+  //       data: {
+  //         dateOfArrival: Date.now(),
+  //         primaryKey: 1
+  //       },
+  //       actions: [
+  //         {action: 'explore', title: 'Explore this new world',
+  //           icon: '/src/images/icons/apple-icon-57x57.png'},
+  //         {action: 'close', title: 'Close notification',
+  //           icon: '/src/images/icons/apple-icon-57x57.png'},
+  //       ]
+  //     };
+  //     reg.showNotification('Hello world!', options);
+  //   });
+  // }
   if ("serviceWorker" in navigator) {
     let option = { body };
     navigator.serviceWorker.ready.then(sw => {
